03bb86d546bfbf04a32d2c15037cdb6c6133a14d
maruf <dev.maruf112@gmail.com>
2022-11-24 07:01:09 +0600

    initial set up done

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+// middleware
+app.use(cors());
+app.use(express.json());
+// port
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("I am running on the home of the Server.");
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.efpjwcu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("database connected");
+  client.close();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
